0b2b89990031202f0b61b644aa8638315c12593b
Vinícius Prado Batista <viniciusprado7606@gmail.com>
2025-03-26 21:20:27 -0300

    Update

--- apps.py
+++ apps.py
@@ -2,18 +2,15 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 
-# Usar PyMySQL como driver
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 
-# Configuração do banco de dados MySQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Vini12348@localhost:3306/loja'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Definição do modelo Produto
 class Produto(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(100), nullable=False)
@@ -34,7 +31,6 @@ def produto_to_dict(produto):
         'preco_unitario': produto.preco_unitario
     }
 
-# Rota para testar a conexão com o banco de dados
 @app.route('/test-db', methods=['GET'])
 def test_db():
     try:
@@ -43,14 +39,12 @@ def test_db():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-# Rota para listar todos os produtos
 @app.route('/produtos', methods=['GET'])
 def get_produtos():
     produtos = Produto.query.all()
-    print("Produtos encontrados:", produtos)  # Veja se produtos estão sendo retornados no terminal
+    print("Produtos encontrados:", produtos)
     return jsonify([produto_to_dict(produto) for produto in produtos])
 
-# Rota para adicionar um novo produto
 @app.route('/produtos', methods=['POST'])
 def add_produto():
     data = request.get_json()
@@ -66,7 +60,6 @@ def add_produto():
     db.session.commit()
     return jsonify(produto_to_dict(novo_produto)), 201
 
-# Rota para atualizar um produto pelo ID
 @app.route('/produtos/<int:id>', methods=['PUT'])
 def update_produto(id):
     produto = Produto.query.get(id)
@@ -84,7 +77,6 @@ def update_produto(id):
     db.session.commit()
     return jsonify(produto_to_dict(produto))
 
-# Rota para excluir um produto pelo ID
 @app.route('/produtos/<int:id>', methods=['DELETE'])
 def delete_produto(id):
     produto = Produto.query.get(id)
